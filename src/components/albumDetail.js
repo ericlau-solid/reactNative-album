21a427a90f7f9650afbc378a9da1d88be098a234
Eric Lau <eric.lau@adaptechsolutions.ca>
2017-04-22 17:08:59 -0700

    Implement to lecture 53 - UI app complete

--- src/components/albumDetail.js
+++ src/components/albumDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './card';
 import CardSection from './cardSection';
 import Button from './button';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   const { 
     headerContentStyle, 
     headerTextStyle,
@@ -34,7 +34,9 @@ const AlbumDetail = ({ album }) => {
         />
       </CardSection>
       <CardSection>
-        <Button onPress={() => console.log('pressed')} />
+        <Button onPress={() => Linking.openURL(url)}>
+          Buy me!
+        </Button>
       </CardSection>
     </Card>
   );
